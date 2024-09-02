18b0478df498877960af4da12da89915ff1f6f76
Paulius Juzenas <pauliusj@stud.ntnu.no>
2024-09-02 11:56:17 +0200

    feat: blinking port b

--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
 /**
  * @file main.c
- * @author Theodor Johansson (theodor.lund.johansson@gmail.com)
+ * @author Theodor Johansson, Paulius Juzenas & William Juul Stensrud
  * @brief
  * @version 0.1
  * @date 2024-09-02
@@ -8,3 +8,22 @@
  * @copyright Copyright (c) 2024
  *
  */
+
+#define F_CPU 4915200
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void) {
+    DDRB = 0xff;
+
+    while(1)
+    {
+        PORTB = 0xff;
+        _delay_ms(500);
+        PORTB = 0x00;   
+        _delay_ms(500);
+    }
+
+    return 0;
+}
