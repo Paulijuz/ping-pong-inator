b5e97623c2995db591d637983e85ce802626c06f
Sanntidsal PC 1 <lol no>
2024-09-02 12:24:52 +0200

    style: auto format

--- src/main.c
+++ src/main.c
@@ -17,11 +17,10 @@
 int main(void) {
     DDRB = 0xff;
 
-    while(1)
-    {
+    while (1) {
         PORTB = 0xff;
         _delay_ms(500);
-        PORTB = 0x00;   
+        PORTB = 0x00;
         _delay_ms(500);
     }
 
