8d8e6935c9b8abf30483b39ed6dcb1e1a51e8255
Sanntidsal PC 1 <lol no>
2024-09-02 15:49:32 +0200

    docs: add thourghough documentation for uart driver

--- src/drivers/uart.c
+++ src/drivers/uart.c
@@ -1,7 +1,21 @@
+/**
+ * @file uart.c
+ * @author Johansson, Juzenas & Stensrud
+ * @brief Uart driver.
+ * @version 0.1
+ * @date 2024-09-02
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
 #include "defines.h"
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
+/**
+ * Initalizes usart 0.
+ */
 void uart_init(unsigned int baud_rate) {
     // TODO: Add option to init uart 1.
     baud_rate = F_CPU / 16 / baud_rate - 1;
@@ -22,7 +36,11 @@ void uart_init(unsigned int baud_rate) {
              (1 << USBS0) |  // Set number of stop bits to 2.
              (3 << UCSZ00);  // Set number of data bits to 8.
 }
-
+/**
+ * @brief Transmits a single char over uart 0.
+ *
+ * @param data
+ */
 void uart_transmit_char(unsigned char data) {
     // Wait for transmit buffer to be empty.
     // We do this by checking wheter or not the data
