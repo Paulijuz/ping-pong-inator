7d3a833bfd4525f65fcbed6e2b21534fa37bf1cf
Theodor Johansson <theodor.lund.johansson@gmail.com>
2024-09-02 09:28:42 +0200

    Default formatting and main files.

---
+++ inc/main.h
@@ -0,0 +1,10 @@
+/**
+ * @file main.h
+ * @author Theodor Johansson (theodor.lund.johansson@gmail.com)
+ * @brief
+ * @version 0.1
+ * @date 2024-09-02
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
