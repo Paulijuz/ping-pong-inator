bfe7f79dc9a6f55d38b490885d45c85a02a34f65
Theodor Johansson <theodor.lund.johansson@gmail.com>
2024-09-23 14:09:07 +0200

    Implemented slider driver

--- inc/drivers/sliders.h
+++ inc/drivers/sliders.h
@@ -1,61 +1,70 @@
-// /**
-//  * @file sliders.h
-//  * @author Theodor Johansson, Paulius Juzenas & William Stensrud
-//  * @brief
-//  * @version 0.1
-//  * @date 2024-09-23
-//  *
-//  * @copyright Copyright (c) 2024
-//  *
-//  */
+/**
+ * @file sliders.h
+ * @author Theodor Johansson, Paulius Juzenas & William Stensrud
+ * @brief
+ * @version 0.1
+ * @date 2024-09-23
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
 
-// #ifndef SLIDERS_H
-// #define SLIDERS_H
+#ifndef SLIDERS_H
+#define SLIDERS_H
 
-// #include <stdint.h>
-// #include <stdbool.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
 
-// #define SLIDER_CALIBRATION_DELAY 1500 // ms, delay between calibration steps (for human interaction
+#include "drivers/adc.h"
 
-// // clang-format off
-// typedef enum SLIDER_DIR {
-//     SLIDER_LEFT,
-//     SLIDER_RIGHT,
-// } e_SLIDER_DIR;
-// // clang-format on
+#define SLIDER_CALIBRATION_DELAY 1500 // ms, delay between calibration steps (for human interaction
 
-// typedef enum SLIDER_INITIALIZATION_STATE {
-//     SLIDER_NO_CALIBRATE,
-//     SLIDER_USE_DEFAULT_CALIBRATION,
-//     SLIDER_CALIBRATE
-// } e_SLIDER_INITIALIZATION_STATE;
+// Slider minimum and maximum
+#define SLIDER_MIN 0
+#define SLIDER_MAX 255
 
-// /**
-//  * @brief Struct for slider configuration
-//  *
-//  */
-// typedef struct slider_config_s {
-//     uint8_t x1_calibration[2];
-//     uint8_t x2_calibration[2];
-//     bool    calibrated;
-// } slider_config_t;
+// clang-format off
+typedef enum SLIDER_DIR {
+    SLIDER_LEFT,
+    SLIDER_RIGHT,
+} e_SLIDER_DIR;
+// clang-format on
 
-// /**
-//  * @brief Struct for raw and calibrated slider values
-//  *
-//  */
-// typedef struct slider_s {
-//     int8_t  x1;
-//     int8_t  x2;
+typedef enum SLIDER_INITIALIZATION_STATE {
+    SLIDER_NO_CALIBRATE,
+    SLIDER_USE_DEFAULT_CALIBRATION,
+    SLIDER_CALIBRATE
+} e_SLIDER_INITIALIZATION_STATE;
 
-//     uint8_t raw_x1;
-//     uint8_t raw_x2;
-// } slider_t;
+/**
+ * @brief Struct for slider configuration
+ *
+ */
+typedef struct slider_config_s {
+    uint8_t x1_calibration[2];
+    uint8_t x2_calibration[2];
+    bool    calibrated;
+} slider_config_t;
 
-// void     slider_calibrate(slider_config_t *config);
-// slider_t slider_read(slider_config_t *config);
-// slider_t slider_read_raw(void);
-// uint8_t  slider_adjust(uint8_t raw, slider_config_t *config);
-// uint8_t  map_uint8(uint8_t x, uint8_t in_min, uint8_t in_max, uint8_t out_min, uint8_t out_max);
+/**
+ * @brief Struct for raw and calibrated slider values
+ *
+ */
+typedef struct slider_s {
+    int8_t  x1;
+    int8_t  x2;
 
-// #endif // SLIDERS_H
+    uint8_t raw_x1;
+    uint8_t raw_x2;
+} slider_t;
+
+void     slider_init(slider_config_t *config, e_SLIDER_INITIALIZATION_STATE state);
+void     slider_calibrate(slider_config_t *config);
+void     slider_calibrate_axis(uint8_t slider, e_SLIDER_DIR dir, slider_config_t *config);
+slider_t slider_read(slider_config_t *config);
+slider_t slider_read_raw(void);
+uint8_t  slider_adjust(uint8_t raw, uint8_t calibration[2]);
+uint8_t  map_uint8(uint8_t x, uint8_t in_min, uint8_t in_max, uint8_t out_min, uint8_t out_max);
+
+#endif // SLIDERS_H
--- src/drivers/sliders.c
+++ src/drivers/sliders.c
@@ -1,159 +1,164 @@
-// /**
-//  * @file sliders.c
-//  * @author Theodor Johansson, Paulius Juzenas & William Stensrud
-//  * @brief
-//  * @version 0.1
-//  * @date 2024-09-23
-//  *
-//  * @copyright Copyright (c) 2024
-//  *
-//  */
+/**
+ * @file sliders.c
+ * @author Theodor Johansson, Paulius Juzenas & William Stensrud
+ * @brief
+ * @version 0.1
+ * @date 2024-09-23
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
 
-// #include "drivers/sliders.h"
+#include "drivers/sliders.h"
 
-// /**
-//  * @brief Calibrate slider
-//  *
-//  */
-// void slider_calibrate(slider_config_t *config) {
-//     printf("\r\n--- Slider calibration ---\r\n");
+/**
+ * @brief Initialize slider configuration
+ *
+ * @param config
+ * @param state
+ */
+void slider_init(slider_config_t *config, e_SLIDER_INITIALIZATION_STATE state) {
+    switch (state) {
+    case SLIDER_NO_CALIBRATE:
+        config->calibrated = true;
+        break;
+    case SLIDER_USE_DEFAULT_CALIBRATION:
+        // memcpy(config->x1_calibration, (uint8_t[2]){0, 255}, 2);
+        // memcpy(config->x2_calibration, (uint8_t[2]){0, 255}, 2);
+        config->x1_calibration[0] = 0;
+        config->x1_calibration[1] = 255;
+        config->x2_calibration[0] = 0;
+        config->x2_calibration[1] = 255;
+        config->calibrated        = true;
+        break;
+    case SLIDER_CALIBRATE:
+        config->calibrated = false;
+        break;
+    }
+}
 
-//     // Slider 1 min calibration
-//     printf("Move slider 1 left\r\n");
-//     _delay_ms(SLIDER_CALIBRATION_DELAY);
-//     printf("Calibrating...\r\n");
-//     uint8_t calx = 0;
-//     for (int i = 0; i < 100; ++i) {
-//         slider_t slider = slider_read_raw();
-//         calx += slider.raw_x1;
+/**
+ * @brief Calibrate slider
+ *
+ */
+void slider_calibrate(slider_config_t *config) {
+    printf("\r\n--- Slider calibration ---\r\n");
 
-//         _delay_ms(10);
-//     }
-//     config->x1_min = calx / 100;
+    // Slider calibration
+    slider_calibrate_axis(0, SLIDER_LEFT, config);
+    slider_calibrate_axis(0, SLIDER_RIGHT, config);
+    slider_calibrate_axis(1, SLIDER_LEFT, config);
+    slider_calibrate_axis(1, SLIDER_RIGHT, config);
 
-//     // Slider 1 max calibration
-//     printf("Move slider 1 right\r\n");
-//     _delay_ms(SLIDER_CALIBRATION_DELAY);
-//     printf("Calibrating...\r\n");
-//     calx = 0;
-//     for (int i = 0; i < 100; ++i) {
-//         slider_t slider = slider_read_raw();
-//         calx += slider.raw_x1;
+    // Check if max and min are swapped
+    if (config->x1_calibration[0] < config->x1_calibration[1]) {
+        uint8_t temp              = config->x1_calibration[1];
+        config->x1_calibration[1] = config->x1_calibration[0];
+        config->x1_calibration[0] = temp;
+    }
+    if (config->x2_calibration[1] < config->x2_calibration[0]) {
+        uint8_t temp              = config->x2_calibration[1];
+        config->x2_calibration[1] = config->x2_calibration[0];
+        config->x2_calibration[0] = temp;
+    }
 
-//         _delay_ms(10);
-//     }
-//     config->x1_max = calx / 100;
+    printf("\r\n--- Calibration complete ---\r\n");
+    printf("Slider 1: %u/%u\r\n", config->x1_calibration[0], config->x1_calibration[1]);
+    printf("Slider 2: %u/%u\r\n", config->x2_calibration[0], config->x2_calibration[1]);
+    _delay_ms(SLIDER_CALIBRATION_DELAY);
+    config->calibrated = true;
+}
 
-//     for (int axis = 0; axis < JOYSTICK_ENUM_LENGTH; ++axis) {
-//         slider_calibrate_axis(axis, config);
-//     }
+/**
+ * @brief Calibrate slider axis
+ *
+ * @param axis
+ * @param config
+ */
+void slider_calibrate_axis(uint8_t slider, e_SLIDER_DIR dir, slider_config_t *config) {
+    char *directions[2];
+    directions[SLIDER_LEFT]  = "left";
+    directions[SLIDER_RIGHT] = "right";
 
-//     // Check if max and min are swapped
-//     if (config->x_config.max < config->x_config.min) {
-//         uint8_t temp         = config->x_config.max;
-//         config->x_config.max = config->x_config.min;
-//         config->x_config.min = temp;
-//     }
-//     if (config->y_config.max < config->y_config.min) {
-//         uint8_t temp         = config->y_config.max;
-//         config->y_config.max = config->y_config.min;
-//         config->y_config.min = temp;
-//     }
+    printf("Move slider %u %s\r\n", slider, directions[dir]);
+    _delay_ms(SLIDER_CALIBRATION_DELAY);
+    printf("Calibrating...\r\n");
+    uint8_t calx = 0;
+    for (int i = 0; i < 100; ++i) {
+        slider_t slider_values = slider_read_raw();
+        if (slider == 1) {
+            calx += slider_values.raw_x1;
+        } else if (slider == 2) {
+            calx += slider_values.raw_x2;
+        }
+        calx += slider_values.raw_x1;
+        _delay_ms(10);
+    }
+    calx /= 100;
 
-//     printf("\r\n--- Calibration complete ---\r\n");
-//     printf("Joystick X: %u/%u/%u\r\n", config->x_config.min, config->x_config.center, config->x_config.max);
-//     printf("Joystick Y: %u/%u/%u\r\n", config->y_config.min, config->y_config.center, config->y_config.max);
-//     _delay_ms(CALIBRATION_DELAY);
-//     config->calibrated = true;
-// }
+    // Store calibration value
+    switch (slider) {
+    case 1:
+        config->x1_calibration[dir] = calx;
+        break;
+    case 2:
+        config->x2_calibration[dir] = calx;
+        break;
+    }
+}
 
-// /**
-//  * @brief Calibrate slider axis
-//  *
-//  * @param axis
-//  * @param config
-//  */
-// void slider_calibrate_axis(uint8_t slider, e_SLIDER_DIR dir, slider_config_t *config) {
-//     char *directions[2];
-//     directions[SLIDER_LEFT]  = "left";
-//     directions[SLIDER_RIGHT] = "right";
+/**
+ * @brief Read slider values
+ *
+ * @param config
+ */
+slider_t slider_read(slider_config_t *config) {
+    slider_t position = slider_read_raw();
+    position.x1       = slider_adjust(position.raw_x1, config->x1_calibration);
+    position.x2       = slider_adjust(position.raw_x2, config->x2_calibration);
+    return position;
+}
 
-//     printf("Move slider %u %s\r\n", slider, directions[dir]);
-//     _delay_ms(SLIDER_CALIBRATION_DELAY);
-//     printf("Calibrating...\r\n");
-//     uint8_t calx = 0;
-//     for (int i = 0; i < 100; ++i) {
-//         slider_t slider_values = slider_read_raw();
-//         if (slider == 1) {
-//             calx += slider_values.raw_x1;
-//         } else if (slider == 2) {
-//             calx += slider_values.raw_x2;
-//         }
-//         calx += slider_values.raw_x1;
-//         _delay_ms(10);
-//     }
-//     calx /= 100;
+/**
+ * @brief Read slider raw values
+ *
+ */
+slider_t slider_read_raw(void) {
+    uint16_t slider_raw = adc_read();
+    slider_t position   = {
+        .x1 = 0,
+        .x2 = 0,
 
-//     // Store calibration value
-//     switch (slider) {
-//     case 1:
-//         config->x1_calibration[dir] = calx;
-//         break;
-//     case 2:
-//         config->x2_calibration[dir] = calx;
-//         break;
-//     }
-// }
+        .raw_x1 = slider_raw & 0xFF,
+        .raw_x2 = (slider_raw >> 8) & 0xFF,
+    };
+    return position;
+}
 
-// /**
-//  * @brief Read slider values
-//  *
-//  * @param config
-//  */
-// slider_t slider_read(slider_config_t *config) {
-//     slider_t position = slider_read_raw();
-//     position.x1       = slider_adjust(position.raw_x1, config);
-//     position.x2       = slider_adjust(position.raw_x2, config);
-//     return position;
-// }
+/**
+ * @brief Adjust slider value
+ *
+ * @param raw
+ * @param config
+ * @return uint8_t
+ */
+uint8_t slider_adjust(uint8_t raw, uint8_t calibration[2]) {
+    uint8_t min = calibration[0];
+    uint8_t max = calibration[1];
 
-// /**
-//  * @brief Read slider raw values
-//  *
-//  */
-// slider_t slider_read_raw(void) {
-//     uint16_t slider_raw = adc_read();
-//     slider_t position   = {
-//         .x1 = 0,
-//         .x2 = 0,
+    if (raw < min) {
+        return min;
+    } else if (raw > max) {
+        return max;
+    } else {
+        return map_uint8(raw, min, max, SLIDER_MIN, SLIDER_MAX);
+    }
+}
 
-//         .raw_x1 = slider_raw & 0xFF,
-//         .raw_x2 = (slider_raw >> 8) & 0xFF,
-//     };
-//     return position;
-// }
-
-// /**
-//  * @brief Adjust slider value
-//  *
-//  * @param raw
-//  * @param config
-//  * @return uint8_t
-//  */
-// uint8_t slider_adjust(uint8_t raw, slider_config_t *config) {
-//     if (raw < config->min) {
-//         return 0;
-//     } else if (raw > config->max) {
-//         return 100;
-//     } else {
-//         return (raw - config->min) * 100 / (config->max - config->min);
-//     }
-// }
-
-// /**
-//  * @brief Map uint8_t
-//  *
-//  */
-// uint8_t map_uint8(uint8_t x, uint8_t in_min, uint8_t in_max, uint8_t out_min, uint8_t out_max) {
-//     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-// }
+/**
+ * @brief Map uint8_t
+ *
+ */
+uint8_t map_uint8(uint8_t x, uint8_t in_min, uint8_t in_max, uint8_t out_min, uint8_t out_max) {
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
