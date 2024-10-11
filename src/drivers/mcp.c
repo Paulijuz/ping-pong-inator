bae1f625c93a1bb30e7e499c690663bc028a366f
Paulius Juzenas <pauliusj@stud.ntnu.no>
2024-10-11 11:11:22 +0200

    feat: add remaining mcp functions

--- src/drivers/mcp.c
+++ src/drivers/mcp.c
@@ -8,3 +8,49 @@
  * @copyright Copyright (c) 2024
  * 
  */
+
+#include "drivers/mcp.h"
+
+char mcp_read_rx_buffer(char buffer) {
+    spi_enable_slave();
+
+    spi_master_transmit(MCP_LOAD_RX(buffer));
+    char data = spi_master_receive();
+
+    spi_disable_slave();
+
+    return data;
+}
+
+void mcp_load_tx_buffer(char buffer, char data) {
+    spi_enable_slave();
+
+    spi_master_transmit(MCP_LOAD_TX_BUFFER(buffer));
+    spi_master_transmit(data);
+
+    spi_disable_slave();
+}
+
+char mcp_read_status(void) {
+    spi_enable_slave();
+
+    spi_master_transmit(MCP_READ_STATUS);
+    char status = spi_master_read();
+    char status_copy = spi_master_read();
+
+    spi_disable_slave();
+
+    return status;
+}
+
+char mcp_rx_status(void) {
+    spi_enable_slave();
+
+    spi_master_transmit(MCP_RX_STATUS);
+    char status = spi_master_read();
+    char status_copy = spi_master_read();
+
+    spi_disable_slave();
+
+    return status;
+}
