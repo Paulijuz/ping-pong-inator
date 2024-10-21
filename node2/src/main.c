8eb3a2b4c455cbe3f5177998ed49e9081dca6b34
Theodor Johansson <theodor.lund.johansson@gmail.com>
2024-10-21 12:01:54 +0200

    feat: CAN messages, both send and receive.

--- node2/src/main.c
+++ node2/src/main.c
@@ -3,6 +3,9 @@
 #include "uart.h"
 #include "sam.h"
 
+#include "can_controller.h"
+#include "time.h"
+
 //Import UART from Node 2 starter code, then edit include path accordingly. Also, remember to update the makefile
 //#include "uart.h"
 
@@ -10,14 +13,27 @@ int main()
 {
     SystemInit();
 
-    WDT->WDT_MR = WDT_MR_WDDIS; //Disable Watchdog Timer
-
+    WDT->WDT_MR = WDT_MR_WDDIS; // Disable Watchdog Timer
     uart_init(F_CPU, 9600);
-    printf("Hello World\n\r");
 
-    while (1)
-    {
-        printf("Hello World\n\r");
+    uint32_t can_br = 0x01293165;
+    uint8_t  status = can_init_def_tx_rx_mb(can_br);
+    if (status) {
+      printf("CAN initialization failed\n\r");
+    } else {
+      printf("CAN initialization successful\n\r");
     }
-    
+
+    uint8_t i = 0;
+    while (1) {
+      CAN0_Handler();
+      CAN_MESSAGE can_msg = {
+        .id          = i++,
+        .data_length = 6,
+        .data        = "world!"
+      };
+      can_send(&can_msg, 0);
+
+      time_spinFor(msecs(1));
+    }
 }
