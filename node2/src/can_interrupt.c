bc0ed1cba37dbef060d516e25ae5b9f63b59285e
Theodor Johansson <theodor.lund.johansson@gmail.com>
2024-11-04 13:17:44 +0100

    style: can_interrupt file indentation

--- node2/src/can_interrupt.c
+++ node2/src/can_interrupt.c
@@ -8,7 +8,6 @@
  *
  */
 
-
 #include <stdio.h>
 
 #include "sam.h"
@@ -29,74 +28,73 @@
  * \retval
  */
 void CAN0_Handler(void) {
-  // if (DEBUG_RX_INTERRUPT)
-  //   printf("CAN0 interrupt\n\r");
-  char can_sr = CAN0->CAN_SR; // CAN Status Register
+    // if (DEBUG_RX_INTERRUPT)
+    //   printf("CAN0 interrupt\n\r");
+    char can_sr = CAN0->CAN_SR; // CAN Status Register
 
-  // Disable global interrupts
-  // cli();
-  // __disable_irq();
-  // NVIC_DisableIRQ(ID_CAN0);
+    // Disable global interrupts
+    // cli();
+    // __disable_irq();
+    // NVIC_DisableIRQ(ID_CAN0);
 
-  // RX interrupt
-  uint8_t can_mailbox_interrupt = can_sr & (CAN_SR_MB1 | CAN_SR_MB2); // Only mailbox 1 and 2 specified for receiving
-  if (can_mailbox_interrupt) {
-    CAN_MESSAGE message;
-    uint8_t     status = 0;
+    // RX interrupt
+    uint8_t can_mailbox_interrupt = can_sr & (CAN_SR_MB1 | CAN_SR_MB2); // Only mailbox 1 and 2 specified for receiving
+    if (can_mailbox_interrupt) {
+        CAN_MESSAGE message;
+        uint8_t     status = 0;
 
-    // Check which mailbox received the message
-    if (can_sr & CAN_SR_MB1) {
-      status |= can_receive(&message, 1);                     // Mailbox 1 event
-    } else if (can_sr & CAN_SR_MB2) {
-      status |= can_receive(&message, 2);                     // Mailbox 2 event
-    } else {
-      printf("CAN0 message arrived in non-used mailbox\n\r"); // Incorrect event
+        // Check which mailbox received the message
+        if (can_sr & CAN_SR_MB1) {
+            status |= can_receive(&message, 1);                     // Mailbox 1 event
+        } else if (can_sr & CAN_SR_MB2) {
+            status |= can_receive(&message, 2);                     // Mailbox 2 event
+        } else {
+            printf("CAN0 message arrived in non-used mailbox\n\r"); // Incorrect event
+        }
+
+        // Check error status
+        if (status != 0) {
+            // Somehow faster CAN messages prevent mailbox busy errors??
+            printf("CAN0 Mailboxes busy\n\r");
+            return;
+        }
+
+        if (DEBUG_RX_INTERRUPT) {
+            printf("ID: %d -> ", message.id);
+            // printf("%d -> ", message.data_length);
+            if (message.data_length > 2) {
+                printf("message too long, id: %d\n\r", message.id);
+            }
+            for (int i = 0; i < message.data_length; i++) {
+                printf("%d ", *((int8_t *)&message.data[i]));
+            }
+            printf("\n\r");
+        }
+
+        if (message.id == 1000) {
+            float pos = *((int8_t *)&message.data[0]) + 128;
+            servo_set_pos(pos / 255);
+        }
     }
 
-    // Check error status
-    if (status != 0) {
-      // Somehow faster CAN messages prevent mailbox busy errors??
-      printf("CAN0 Mailboxes busy\n\r");
-      return;
-    }
+    if (can_sr & CAN_SR_MB0) {
+        if (DEBUG_TX_INTERRUPT) {
+            printf("CAN0 MB0 ready to send \n\r");
+        }
 
-    if (DEBUG_RX_INTERRUPT) {
-      // printf("ID: %d -> ", message.id);
-      // printf("%d -> ", message.data_length);
-      if (message.data_length > 2) {
-        printf("message too long, id: %d\n\r", message.id);
-      }
-      for (int i = 0; i < message.data_length; i++) {
-        // printf("%d ", *((int8_t *)&message.data[i]));
-      }
-      // printf("\n\r");
+        // Disable interrupt
+        // CAN0->CAN_IDR = CAN_IER_MB0;
     }
-
-    if (message.id == 1000) {
-      float pos = *((int8_t *)&message.data[0]);
-      motor_set_speed(pos/128);
-      servo_set_pos((pos + 128)/256);
-    }
-  }
-
-  if (can_sr & CAN_SR_MB0) {
-    if (DEBUG_TX_INTERRUPT) {
-      printf("CAN0 MB0 ready to send \n\r");
-    }
-
-    // Disable interrupt
-    // CAN0->CAN_IDR = CAN_IER_MB0;
-  }
 
     if (can_sr & CAN_SR_ERRP) {
-      if (DEBUG_BUS_ERROR) {
-        printf("CAN0 ERRP error\n\r");
-      }
+        if (DEBUG_BUS_ERROR) {
+            printf("CAN0 ERRP error\n\r");
+        }
     }
     if (can_sr & CAN_SR_TOVF) {
-      if (DEBUG_BUS_ERROR) {
-        printf("CAN0 timer overflow\n\r");
-      }
+        if (DEBUG_BUS_ERROR) {
+            printf("CAN0 timer overflow\n\r");
+        }
     }
 
     // NVIC_ClearPendingIRQ(ID_CAN0);
