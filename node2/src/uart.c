a177ae0dbbbeea46970d9deb899d5df1464fc505
Theodor Johansson <theodor.lund.johansson@gmail.com>
2024-11-11 09:55:41 +0100

    feat: add logging functionality with log levels and macros

--- node2/src/uart.c
+++ node2/src/uart.c
@@ -1,113 +1,109 @@
-#include "sam.h"
 #include <stdio.h>
+#include "sam.h"
+#include "logger.h"
 
 typedef struct RingBuf {
     uint8_t buffer[1024];
-    int capacity;
-    
-    int insertIdx;
-    int removeIdx;
-    int length;
+    int     capacity;
+
+    int     insertIdx;
+    int     removeIdx;
+    int     length;
 } RingBuf;
 
 static RingBuf ring_buf = {0};
 
-
-int push(RingBuf* rb, uint8_t val){
-    if(rb->length >= (sizeof(rb->buffer)/sizeof(rb->buffer[0]))){
+int            push(RingBuf *rb, uint8_t val) {
+    if (rb->length >= (sizeof(rb->buffer) / sizeof(rb->buffer[0]))) {
         return 0;
     }
     rb->buffer[rb->insertIdx] = val;
-    rb->insertIdx = (rb->insertIdx + 1) % (sizeof(rb->buffer)/sizeof(rb->buffer[0]));
+    rb->insertIdx             = (rb->insertIdx + 1) % (sizeof(rb->buffer) / sizeof(rb->buffer[0]));
     rb->length++;
     return 1;
 }
 
-int pop(RingBuf* rb, uint8_t* val){    
-    if(!rb->length){
+int pop(RingBuf *rb, uint8_t *val) {
+    if (!rb->length) {
         return 0;
     }
-    *val = rb->buffer[rb->removeIdx];
-    rb->removeIdx = (rb->removeIdx + 1) % (sizeof(rb->buffer)/sizeof(rb->buffer[0]));
+    *val          = rb->buffer[rb->removeIdx];
+    rb->removeIdx = (rb->removeIdx + 1) % (sizeof(rb->buffer) / sizeof(rb->buffer[0]));
     rb->length--;
     return 1;
 }
 
-void uart_init(uint32_t cpufreq, uint32_t baudrate){
+void uart_init(uint32_t cpufreq, uint32_t baudrate) {
     PMC->PMC_PCER0 |= (1 << ID_UART);
-    
+
     // Set UART pins (A8, A9) to use alternate function (this disables regular IO)
-    PIOA->PIO_PDR   |=   PIO_PA8 | PIO_PA9;
+    PIOA->PIO_PDR |= PIO_PA8 | PIO_PA9;
     // Set alternate function A (see tables 9-2, 34-2)
-    PIOA->PIO_ABSR  &= ~(PIO_PA8 | PIO_PA9);
-    
+    PIOA->PIO_ABSR &= ~(PIO_PA8 | PIO_PA9);
+
     // Configure UART settings
-    UART->UART_CR   |= UART_CR_TXEN | UART_CR_RXEN;
-    UART->UART_MR   |= UART_MR_PAR_NO;
+    UART->UART_CR |= UART_CR_TXEN | UART_CR_RXEN;
+    UART->UART_MR |= UART_MR_PAR_NO;
     UART->UART_BRGR = cpufreq / 16 / baudrate;
-    
-    
+
     // Configure interrupts on receive ready and errors
     UART->UART_IDR = 0xFFFFFFFF;
     UART->UART_IER = UART_IER_RXRDY | UART_IER_OVRE | UART_IER_FRAME | UART_IER_PARE;
 
     // Enable UART interrupt in the Nested Vectored Interrupt Controller (NVIC)
-    NVIC_EnableIRQ((IRQn_Type) ID_UART);
-    
-}    
+    NVIC_EnableIRQ((IRQn_Type)ID_UART);
+}
 
-void uart_tx(uint8_t val){
-    while(!(UART->UART_SR & UART_SR_TXEMPTY)){}
+void uart_tx(uint8_t val) {
+    while (!(UART->UART_SR & UART_SR_TXEMPTY)) {
+    }
     UART->UART_THR = val;
 }
 
-uint8_t uart_rx(uint8_t* val){
+uint8_t uart_rx(uint8_t *val) {
     return pop(&ring_buf, val);
-}    
+}
 
-int uart_flush(char* buf, int len){
+int uart_flush(char *buf, int len) {
     int r = 0;
-    for(; r < len; r++){
-        int ret = uart_rx((uint8_t*)&buf[r]);
-        if(!ret){
+    for (; r < len; r++) {
+        int ret = uart_rx((uint8_t *)&buf[r]);
+        if (!ret) {
             break;
-        }                
-    }    
-    return r;
-}    
-
-
-void UART_Handler(){
-    
-    uint32_t status = UART->UART_SR;
-    
-    // Errors: Reset UART
-    if(status & (UART_SR_OVRE | UART_SR_FRAME | UART_SR_PARE)){
-        UART->UART_CR = UART_CR_RXEN | UART_CR_TXEN | UART_CR_RSTSTA;
-    }
-    
-    // Receive ready: push to ring buffer
-    if(status & UART_SR_RXRDY){
-        if(!push(&ring_buf, UART->UART_RHR & 0xff)){
-            printf("UART receive buffer full\n");
         }
     }
-    
-} 
+    return r;
+}
 
+void UART_Handler() {
 
+    uint32_t status = UART->UART_SR;
+
+    // Errors: Reset UART
+    if (status & (UART_SR_OVRE | UART_SR_FRAME | UART_SR_PARE)) {
+        UART->UART_CR = UART_CR_RXEN | UART_CR_TXEN | UART_CR_RSTSTA;
+    }
+
+    // Receive ready: push to ring buffer
+    if (status & UART_SR_RXRDY) {
+        if (!push(&ring_buf, UART->UART_RHR & 0xff)) {
+            log_warning("UART receive buffer full");
+            // printf("UART receive buffer full\n");
+        }
+    }
+}
 
 // See https://interrupt.memfault.com/blog/boostrapping-libc-with-newlib
 
 extern int _end;
 #include <sys/stat.h>
 
-void *_sbrk(int incr){
+void *_sbrk(int incr) {
     static unsigned char *heap = NULL;
-    unsigned char *prev_heap;
+    unsigned char        *prev_heap;
 
-    if(heap == NULL){
-        heap = (unsigned char*)&_end;
+    if (heap == NULL) {
+        heap = (unsigned char *)&_end;
     }
     prev_heap = heap;
 
@@ -116,61 +112,58 @@ void *_sbrk(int incr){
     return prev_heap;
 }
 
-int _close(int file){
+int _close(int file) {
     return -1;
 }
 
-int _fstat(int file, struct stat* st){
+int _fstat(int file, struct stat *st) {
     st->st_mode = S_IFCHR;
     return 0;
 }
 
-int _isatty(int file){
+int _isatty(int file) {
     return 1;
 }
 
-int _lseek(int file, int ptr, int dir){
+int _lseek(int file, int ptr, int dir) {
     return 0;
 }
 
-__attribute__((naked)) void _exit(int status){
+__attribute__((naked)) void _exit(int status) {
     //__asm("BKPT #0");
 }
 
-void _kill(int pid, int sig){
+void _kill(int pid, int sig) {
     return;
 }
 
-int _getpid(void){
+int _getpid(void) {
     return -1;
 }
 
-int _write(int file, char* ptr, int len){
-    if(file > 1){
-        return -1;    
+int _write(int file, char *ptr, int len) {
+    if (file > 1) {
+        return -1;
     }
 
-    for(int idx = 0; idx < len; idx++){
+    for (int idx = 0; idx < len; idx++) {
         uart_tx((uint8_t)ptr[idx]);
     }
     return len;
 }
 
-int _read(int file, char* ptr, int len){
-    if(file > 1){
+int _read(int file, char *ptr, int len) {
+    if (file > 1) {
         return -1;
     }
-    
+
     int nread = 0;
-    for(int idx = 0; idx < len; idx++){
-        int b = uart_rx((uint8_t*)&ptr[idx]);
-        nread += b;     
-        if(!b){
+    for (int idx = 0; idx < len; idx++) {
+        int b = uart_rx((uint8_t *)&ptr[idx]);
+        nread += b;
+        if (!b) {
             return nread;
-        }        
+        }
     }
     return nread;
 }
-
-
-   
